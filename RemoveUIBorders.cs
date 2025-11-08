e5496285dc529aa6855103726dfda3b37e7d35f6
lucas-xk <82394206+lucas-xk@users.noreply.github.com>
2025-11-08 15:51:35 -0300

    v1.0.0
    Initial release

---
+++ RemoveUIBorders.cs
@@ -0,0 +1,77 @@
+﻿using BepInEx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace remove_ui_borders
+{
+    [BepInPlugin("lucasxk.erenshor.removeuiborders", "Remove UI Borders", "1.0.0")]
+    public class RemoveUIBorders : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private void OnDestroy()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+
+        private string sceneName;
+
+        private bool alreadyDestroyed = false;
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            sceneName = scene.name;
+
+            if (sceneName == "Menu" || sceneName == "LoadScene")
+                return;
+
+            var borderObj = GameObject.Find("UI/UIElements/Canvas/HotbarPar/Image (1)");
+            if (borderObj != null)
+            {
+                GameObject.Destroy(borderObj);
+            }
+
+            borderObj = GameObject.Find("UI/UIElements/NewGroupPar/Image (4)");
+            if (borderObj != null)
+            {
+                GameObject.Destroy(borderObj);
+            }
+
+            borderObj = GameObject.Find("UI/UIElements/PlayerLifePar/Image (3)");
+            if (borderObj != null)
+            {
+                GameObject.Destroy(borderObj);
+            }
+
+            borderObj = GameObject.Find($"UI/UIElements/CharmedPar/CharmedNPC");
+            var borderComp = borderObj.GetComponent("UnityEngine.UI.Image");
+            Object.Destroy(borderComp);
+                        
+            borderObj = GameObject.Find("UI/UIElements/TargCanv/NewTargetWindow/Image (1)");
+            if (borderObj != null)
+            {
+                GameObject.Destroy(borderObj);
+            }
+
+            borderObj = GameObject.Find("UI/UIElements/TargCanv/NewTargetWindow/LifeNumBG (1)");
+            if (borderObj != null)
+            {
+                GameObject.Destroy(borderObj);
+            }
+
+            if (alreadyDestroyed)
+            {
+                return;
+            }
+            borderObj = GameObject.Find("UI/UIElements/TargCanv/NewTargetWindow/Image");
+            if (borderObj != null)
+            {
+                GameObject.Destroy(borderObj);
+                alreadyDestroyed = true;
+            }
+        }
+    }
+}
